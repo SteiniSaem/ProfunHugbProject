fd0cb55bf0434ba1101b8694ed414773a5884fca
Ragnheiður Sveinsdóttir <ragnh94@gmail.com>
2021-04-12 11:47:57 +0000

    wip

---
+++ srcAutograder/NegativeOrZeroParameterException.java
@@ -0,0 +1,8 @@
+public class NegativeOrZeroParameterException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public NegativeOrZeroParameterException(String message) {
+        super(message);
+    }
+}
